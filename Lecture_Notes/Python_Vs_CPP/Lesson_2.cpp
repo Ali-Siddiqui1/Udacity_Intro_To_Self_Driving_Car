c86583d0a2c2431165797767b5edd5fef2132609
Babar Siddiqui <shahrukhsidd577@gmail.com>
2019-09-04 11:50:34 +1000

    C++ Functions

---
+++ Lecture_Notes/Python_Vs_CPP/Lesson_2.cpp
@@ -0,0 +1,57 @@
+/*
+ Functions: Python Vs C++
+ 
+ *Functions have the same role in both Python and C++
+ *Function group statements together to perform some task
+ *Function help you avoid coping and pasting the same code
+ 
+        #The syntax for writing function is slightly different#
+ 
+ ** Python detects the end of a code line based on seeing a carriage return and new line feed. C++ uses a semi-colon for same purpose
+ 
+ ** Python uses indentation to group code statemets together, but C++ uses curly brackets
+ 
+ */
+
+// below function takes in a velocity and time.These are multiplied together to calculate a distance.
+
+
+/*              *** Python   ***
+
+def distance(velocity,time_elapsed):
+    return velocity * time_elapsed
+ 
+print(distance(5,4))
+print(distance(12.1,7.9))
+ */
+
+//                  **CPP**             //
+
+// Import input/output standard library
+#include<iostream>
+
+// function accept two float variable and return a float
+float distance(float velocity, float time_elapsed);
+
+// All CPP programs require a main() function that returns a zero.
+//The main() function calls the distance and output the results to the terminal
+int main(){
+    
+    std::cout << distance(5,4) << std::endl;
+    std::cout << distance(12.1,7.9) << std::endl;
+    
+}
+// And now you have the function definition
+float distance(float velocity, float time_elapsed){
+    return velocity * time_elapsed;
+}
+// Note *** The Only diff is that the main function does not accept any arguments and return an integer of value zero. On the other hand, the distance function accept two floats and return a float
+
+
+////        ******     END      ******  /////
+
+
+
+
+
+
