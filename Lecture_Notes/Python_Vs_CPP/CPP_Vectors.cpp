d85e6770cc0787aa9d6a15a77fe9bc2f8392197b
Babar Ali <shahrukhsidd577@gmail.com>
2019-09-08 07:43:00 +1000

    explained CPP vectors

--- Lecture_Notes/Python_Vs_CPP/CPP_Vectors.cpp
+++ Lecture_Notes/Python_Vs_CPP/CPP_Vectors.cpp
@@ -1,8 +1,40 @@
+// CPP vectors are just like Python list but CPP also has list which but doesn't work the same way as python does
+// CPP list and Python list are both in family structure called sequence containers. These containers allows us to store values in series and then access those values
 //
-//  CPP_Vectors.cpp
-//  
-//
-//  Created by shahrukh siddiqui on 6/9/19.
-//
+// include file from standard library
+#include<vector>
+#include<iostream>
+#include<vector>
+#include<string>
+// Declaring CPP Vectors
+typedef variablename;
+// also need to declare what kind of values will go inside the vector such as integer, char, float, string etc
 
-#include <stdio.h>
+int main(){
+    std::cout<char> charactervectorvariable;
+    std::cout<int> intvectorvariable;
+    std::cout<float>floatvectorvariable;
+    std::cout<double>doublevectorvariable;
+    // more generally you can declare a vector with
+    //std::vector <<datatype<<variablename;
+
+    return 0;
+}
+
+// Practice CPP Vector Library
+//
+// import the vector library
+#include<vector>
+#include<iostream>
+//  write a program that declares three integer vectors named:
+int main(){
+    // vector 1
+    std::vector<int> vector1;
+    // vector 2
+    std::vector<int> vector2;
+    //vector 3
+    std::vector<int> vector3;
+
+    return 0;
+
+}
