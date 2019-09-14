ac812fec4e1ee0ebf256f327504726e7ffd1126b
Babar Siddiqui <shahrukhsidd577@gmail.com>
2019-09-14 17:52:55 +1000

    Example of Assign and push_back

--- Lecture_Notes/Python_Vs_CPP/CPP_Vectors.cpp
+++ Lecture_Notes/Python_Vs_CPP/CPP_Vectors.cpp
@@ -141,9 +141,9 @@ intvariable.size()
 
 // main function
 int main(){
-    
+
     vector<float> example;
-    
+
     // initiate a vector
     for(int i = 0;i < 5; i++){
         example.Push_back(i*5.231);
@@ -169,3 +169,46 @@ int x = ++i; // x = 5, i = 6 (called prefix)
 // In both cases, the i variable increases by 1.
 
 //                      An example
+
+//include the iostream and vector libraries
+#include<iostream>
+#include<vector>
+// Use the standard namespace
+using namespace std;
+
+// CPP main function
+int main() {
+    
+    // Part 1: declare and define a vector with values
+    //        {5.0, 5.0, 5.0} and print
+    //         the vector to the terminal using cout
+    // Hint: the syntax vector<datatype> varname(length, value) might help
+    vector<float>length(3,5.0);
+    for(int i = 0; i < length.size(); i++){
+        cout << length[i] << "";
+    }
+    cout << endl;
+    // Part 2: Use push back to add the values 3.0, 2.5, 1.4
+    //      to the back of the vector
+    length.push_back(3.0);
+    length.push_back(2.5);
+    length.push_back(1.4);
+    // Part 3: Print out the vector again using cout
+    
+    for(int i = 0; i < length.size(); i++){
+        cout << length[i] << "";
+    }
+    cout << "\n";
+    // Part 4: Use the vector assign method to override the current vector.
+    // The overriden vector should have 3 elements with
+    // the values {5.0, 5.0, 5.0}
+    length.assign(3,5.0);
+    // Part 5: Print out the vector
+    
+    for(int i = 0; i < length.size(); i++){
+        cout << length[i] << "";
+    }
+    cout <<"\n";
+    
+    return 0;
+}
