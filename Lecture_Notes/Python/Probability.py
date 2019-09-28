ea4f75b84495ba0fcf4465579d00a5378370bb7d
Babar Siddiqui <shahrukhsidd577@gmail.com>
2019-09-29 09:27:43 +1000

    Data Types, Variables, string, numbers and Boolean

---
+++ Lecture_Notes/Python/Probability.py
@@ -0,0 +1,49 @@
+#            ""              Python Variables                   ""
+# Python variables values are assigned here with variable name p_head 
+p_head = 0.5
+# print statement use 
+print("Probability of p_head is:",p_head)
+
+# Python variables "p_tail" are used here
+p_tail = 1.0 - p_head 
+# printout probability of trail
+print("Probability of p_tail is: ",p_tail)
+
+
+#           ""              Python Data Types           ""
+
+# Here String, numbers and boolean are defined 
+"string" # declaring strings with double qoutes
+print('single string')
+my_string1 = 'string with single qoute'
+my_string2 = "string with double qoute"
+print('\n') # for new line
+print(my_string1)
+print(my_string2)
+
+# Numbers And BOOLEANS 
+print("Numbers \n")
+my_float = 0.5
+
+print(my_float,"\n")
+
+my_int = 1 
+print(my_int,"\n")
+
+negative = -11
+print(negative,"\n")
+
+fraction = 1/2 
+print(fraction,"\n")
+
+# for boolean 
+check_boolean = my_int == negative 
+print(check_boolean,'\n')
+
+
+
+
+
+
+
+
